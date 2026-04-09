a69fcbf6926cfe25fdbbd6ed33f61a492460e620
tedeyang <tedeyang@gmail.com>
2026-04-09 22:40:00 +0800

    Bump version to v0.4.0

--- __version__.py
+++ __version__.py
@@ -1,6 +1,6 @@
 """Version information for Wyckoff VPA Skill"""
 
-__version__ = "0.3.4"
+__version__ = "0.4.0"
 __author__ = "tedeyang"
 __description__ = "Wyckoff VPA analysis for Chinese A-shares"
 __license__ = "MIT"
